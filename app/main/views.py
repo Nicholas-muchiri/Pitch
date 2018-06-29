68ce6b647cf3c23965b97366fcc16015e47950ba
Nicholas-muchiri <nickromero187@gmail.com>
2018-06-29 10:27:38 +0300

    changes made to index.html

--- app/main/views.py
+++ app/main/views.py
@@ -20,7 +20,6 @@ def category(id):
     view category function that returns the pitches of that category
     '''
     category = Category.query.get(id)
-    title = f'{category.name} pitches'
     pitch = Pitch.get_pitches(category.id)
 
     return render_template('category.html', title = title, category = category, pitch = pitch)
