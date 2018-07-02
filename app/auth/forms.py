812c1f1d8241d8fe35f367170b135b99bc1a47a7
Nicholas-muchiri <nickromero187@gmail.com>
2018-07-02 16:19:11 +0300

    forms.py updated

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,7 +1,7 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError
+from wtforms import ,ValidationError, StringField, SubmitField, BooleanField, PasswordField
 from wtforms.validators import Required, Email, EqualTo
 from ..models import User
+from flask_wtf import FlaskForm
 
 class RegistrationForm(FlaskForm):
     first_name = StringField('Enter your First Name', validators = [Required()])
@@ -13,16 +13,16 @@ class RegistrationForm(FlaskForm):
     password_confirm = PasswordField('Confirm Passwords', validators = [Required()])
     submit = SubmitField('Sign Up')
 
-    def validate_email(self, data_field):
-            if User.query.filter_by(email = data_field.data).first():
-                raise ValidationError('There is an account with that email')
-    
     def validate_username(self, data_field):
         if User.query.filter_by(username = data_field.data).first():
             raise ValidationError('That username is taken')
 
+    def validate_email(self, data_field):
+            if User.query.filter_by(email = data_field.data).first():
+                raise ValidationError('That account has been taken')
+    
 class LoginForm(FlaskForm):
     email = StringField('Your Email Address', validators = [Required(), Email()])
     password = PasswordField('Password', validators = [Required()])
-    remember = BooleanField('Remember me')
+    remember = BooleanField('Remember my password.')
     submit = SubmitField('Sign In')
