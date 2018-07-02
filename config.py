5f92c03322aeea179b25245591c58514c86717bd
Nicholas-muchiri <nickromero187@gmail.com>
2018-07-02 16:31:06 +0300

    config.py updated

--- config.py
+++ config.py
@@ -1,22 +1,16 @@
 import os
 
 class Config:
-    '''
-    general configuration
-    '''
+
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class ProdConfig(Config):
-    '''
-    production configuration child class
-    '''
+
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class DevConfig(Config):
-    '''
-    development configuration child class
-    '''
+    
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://nicholas:moringa@localhost/pitch'
     DEBUG = True
 
