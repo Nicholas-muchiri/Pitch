bbd3af01a980fcf57c5b09a4b871014561082103
Nicholas-muchiri <nickromero187@gmail.com>
2018-07-02 16:36:05 +0300

    test_user.py updated

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,17 +1,21 @@
-import unittest
 from app.models import User
+import unittest
 
 class UserModelTest(unittest.TestCase):
-    def setUp(self):
-        self.new_user = User(password = 'password')
+
+	 def test_password_verification(self):
+        self.assertTrue(self.new_user.verify_password('test_password_verification'))
 
     def test_password_setter(self):
         self.assertTrue(self.new_user.pass_secure is not None)
 
     def test_no_access_password(self):
-            with self.assertRaises(AttributeError):
-                self.new_user.password
+        with self.assertRaises(AttributeError):
+        self.new_user.password
 
-        def test_password_verification(self):
-            self.assertTrue(self.new_user.verify_password('password'))
+    def setUp(self):
+        self.new_user = User(password = 'password')    
+
+     def test_password_verification(self):
+        self.assertTrue(self.new_user.verify_password('password'))
             
